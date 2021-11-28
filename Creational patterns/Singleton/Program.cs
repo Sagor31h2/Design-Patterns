c3cb186cf38599ec2be1f4fbdc4c06c2142d74aa
Sagor31h2 <sagor31h2@gmail.com>
2021-11-29 00:19:08 +0600

    Singleton DP

---
+++ Creational patterns/Singleton/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton fromTeachaer = Singleton.GetInstance;
+            fromTeachaer.PrintDetails("From Teacher");
+
+            Singleton fromStudent = Singleton.GetInstance;
+            fromStudent.PrintDetails("From Student");
+            Console.ReadLine();
+        }
+    }
+}
